af0f653d53200338e3b338164529367af09a6c56
wendy <113089770+wendyychenn@users.noreply.github.com>
2023-09-18 18:24:01 +0000

    Update Boxes.js

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,9 +1,21 @@
 import "./boxes.css";
 
 function Boxes() {
+  function handleInnerClick(event) {
+    const element = event.target;
+    element.style.backgroundColor = "cyan";
+  }
+
+  function handleOuterClick(event) {
+    const element = event.currentTarget;
+    setTimeout(function () {
+      element.style.backgroundColor = "red";
+    }, 2000);
+  }
+
   return (
-    <div className="outer">
-      <div className="inner"></div>
+    <div className="outer" onClick={handleOuterClick}>
+      <div className="inner" onClick={handleInnerClick}></div>
     </div>
   );
 }
