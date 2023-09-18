6c7552bc1a74c164710ef534b66481e376da2a7f
wendy <113089770+wendyychenn@users.noreply.github.com>
2023-09-18 18:13:50 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./styles.css";
 
+import Boxes from "./components/Boxes";
+
 export default function App() {
+  function handleClick() {
+    alert("You clicked me!");
+  }
+
+  function handleMouseOver(event) {
+    const element = event.target; //target means <button></button>
+    element.textContent = "You are hovering above me";
+  }
+
+  function handleMouseOut(event) {
+    const element = event.target;
+    element.textContent = "Click Me!";
+  }
+
   return (
     <div className="App">
       <p>This is a test project.</p>
+      <button
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Click Me
+      </button>
+      <Boxes />
     </div>
   );
 }
+
+// npm install gh-pages --save-dev
+// npm run deploy
+
+//<button onClick={() => alert("You clicked me!")}>Click Me</button>
---
+++ src/components/Boxes.js
@@ -0,0 +1,11 @@
+import "./boxes.css";
+
+function Boxes() {
+  return (
+    <div className="outer">
+      <div className="inner"></div>
+    </div>
+  );
+}
+
+export default Boxes;
